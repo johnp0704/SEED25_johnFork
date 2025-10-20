cd30d8cd0477febfab3974e5a06fbde6d0fba7b8
Niels Keller <niels.keller1@gmail.com>
2025-10-20 13:35:56 -0400

    Added ramping speeds to test

--- motor_Testing/sabertooth_testing.py
+++ motor_Testing/sabertooth_testing.py
@@ -4,7 +4,15 @@ import time
 
 motor = st.SaberToothMotorDriver(True,True)
 
-for i in range(-30,30):
+for i in range(0, -30):
+    motor.updateMotorSpeed(i,i)
+    time.sleep(0.2)
+
+for i in range(-30, 30):
+    motor.updateMotorSpeed(i,i)
+    time.sleep(0.2)
+
+for i in range(30, 0):
     motor.updateMotorSpeed(i,i)
     time.sleep(0.2)
 
