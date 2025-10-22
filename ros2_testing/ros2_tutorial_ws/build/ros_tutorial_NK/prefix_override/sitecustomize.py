5f88b271d4c142be8ed7d979dcfa04fb93f1dc41
Niels Keller <niels.keller1@gmail.com>
2025-10-22 16:09:12 -0400

    At end of ROS tutorial 4 https://youtu.be/iBGZ8LEvkCY?list=PLLSegLrePWgJudpPUof4-nVFHGkB62Izy

---
+++ ros2_testing/ros2_tutorial_ws/build/ros_tutorial_NK/prefix_override/sitecustomize.py
@@ -0,0 +1,4 @@
+import sys
+if sys.prefix == '/usr':
+    sys.real_prefix = sys.prefix
+    sys.prefix = sys.exec_prefix = '/home/nk/Desktop/SEED25/ros2_testing/ros2_tutorial_ws/install/ros_tutorial_NK'
