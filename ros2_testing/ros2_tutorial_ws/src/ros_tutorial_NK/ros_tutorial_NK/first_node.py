f7783e452ca11eb48b3a68cd32b44f4485738174
Niels Keller <niels.keller1@gmail.com>
2025-10-23 19:22:46 -0400

    ROS tutorial: https://youtu.be/Gg25GfA456o?t=3802

--- ros2_testing/ros2_tutorial_ws/src/ros_tutorial_NK/ros_tutorial_NK/first_node.py
+++ ros2_testing/ros2_tutorial_ws/src/ros_tutorial_NK/ros_tutorial_NK/first_node.py
@@ -6,7 +6,14 @@ class Basic_Node(Node):
     #Define new class which inherits from RCLPY node
     def __init__(self):
         super().__init__("node_one")
-        self.get_logger().info("test2!")
+        self.get_logger().info("Starting Node!")
+        self.counter = 0
+        self.create_timer(1.0, self.timer_callback)
+        
+
+    def timer_callback(self):
+        self.get_logger().info("Test" + str(self.counter))
+        self.counter += 1
 
 
  
