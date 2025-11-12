6230a6c908cf0275ddace60d9a6bcd9979f48311
Niels Keller <niels.keller1@gmail.com>
2025-11-12 17:13:31 -0500

    gains + thres

--- ros_packages/seed_controller_ws/src/mocap_listener/mocap_listener/gtg_node_nk.py
+++ ros_packages/seed_controller_ws/src/mocap_listener/mocap_listener/gtg_node_nk.py
@@ -14,14 +14,14 @@ import matplotlib.pyplot as plt
 GOAL_MARKER_INDEX = 5
 MAX_ACUTUATOR_INPUT = 30
 S_MAX = (MAX_ACUTUATOR_INPUT-10) * 0.6
-GOAL_THRESH = 0.25 # m
+GOAL_THRESH = 0.3 # m
 ANGLE_THRESH = np.deg2rad(10)
 
 REFRESH_RATE = 10 #hz
 R_wheel = .08 #cm
 L = .178 #cm
 K_e = 30
-K_theta = -K_e * 10
+K_theta = -K_e * 20
 
 
 class ControllerNode(Node):
