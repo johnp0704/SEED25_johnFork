534c4167b0d4521ee22e94c76d2084fd957ca111
Niels Keller <niels.keller1@gmail.com>
2026-01-26 19:08:23 -0500

    fixed trapezoidal speed curve

--- tool/tool_pulse_test/tool_pulse_test.ino
+++ tool/tool_pulse_test/tool_pulse_test.ino
@@ -1,14 +1,21 @@
-
 #include <Arduino.h>
 #include "driver/rmt.h"
 
 #define STEP_GPIO      18
-#define DIR_GPIO     4
+#define DIR_GPIO       4
 #define RMT_CHANNEL    RMT_CHANNEL_0
 
 #define RMT_CLK_DIV    80          // 80 MHz / 80 = 1 MHz (1 µs tick)
 #define STEP_FREQ_HZ   10000       // 10 kHz default
 #define STEP_COUNT     2000
+
+
+//Acceleration profile in hz
+#define ACC 25
+#define LOW_SPEED 500
+#define HIGH_SPEED 5000
+#define ACC_DISC_INTERVAL 10 // How many steps to go before changing velocity
+
 
 void rmtStepperInit(){
     rmt_config_t config = {};
@@ -44,6 +51,67 @@ void stepperMove(uint32_t steps, uint32_t freq_hz){
     rmt_wait_tx_done(RMT_CHANNEL, portMAX_DELAY);
 }
 
+
+
+void stepperMoveAccTrap(uint32_t steps){
+    uint16_t f = LOW_SPEED;
+    uint32_t period_us = 1000000UL / f;
+    uint32_t half_period = period_us / 2;
+    uint32_t curve_length = steps/2;
+
+    rmt_item32_t step;
+    step.level0 = 0;
+    step.duration0 = half_period;
+    step.level1 = 1;
+    step.duration1 = half_period;
+
+    // Make sure we have time to stop
+    uint32_t acc_steps = min((int)curve_length, (HIGH_SPEED-LOW_SPEED)/(ACC/ACC_DISC_INTERVAL));
+
+
+    uint32_t vel_change_steps = acc_steps/ACC_DISC_INTERVAL; //How many velocity steeps
+    uint32_t coast_steps = steps - 2*(vel_change_steps*ACC_DISC_INTERVAL); //recalculate acc steps to account for int rounding
+ 
+
+    for (uint16_t i = 0; i < vel_change_steps; i++) {
+        for (uint16_t i = 0; i < ACC_DISC_INTERVAL; i++){
+            rmt_write_items(RMT_CHANNEL, &step, 1, true);
+            
+        }
+        //Update speed at increment
+        f += ACC;
+        half_period = (1000000UL / f) /2;
+
+        step.duration0 = half_period;
+        step.duration1 = half_period;   
+    }
+
+  
+    for (uint16_t i = 0; i < coast_steps; i++){
+        rmt_write_items(RMT_CHANNEL, &step, 1, true);
+        
+    }
+  
+    
+
+    for (uint16_t i = 0; i < vel_change_steps; i++) {
+        for (uint16_t i = 0; i < ACC_DISC_INTERVAL; i++){
+            rmt_write_items(RMT_CHANNEL, &step, 1, true);
+            
+        }
+        //Update speed at increment
+        f -= ACC;
+        half_period = (1000000UL / f) /2;
+
+        step.duration0 = half_period;
+        step.duration1 = half_period;   
+    }
+
+    rmt_wait_tx_done(RMT_CHANNEL, portMAX_DELAY);
+}
+
+
+
 void setup()
 {
     pinMode(DIR_GPIO, OUTPUT);
@@ -66,11 +134,8 @@ void loop()
     
     delay(100);
 
-    stepperMove(100, 500);  // 0.5 
-    stepperMove(100, 1000);  // 1 kHz
-    stepperMove(2000, 2000);  // 2 kHz
-    stepperMove(100, 1000);  // 1 kHz
-    stepperMove(100, 500);  // 0.5 kHz
+    // stepperMove(1000, 500);  // 0.5 
+    stepperMoveAccTrap(20000);
 
     delay(1000);
 
