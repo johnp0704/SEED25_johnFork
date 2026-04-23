597558d0cb0afcc2952b3056e9542f2e0c7e88a3
Niels Keller <niels.keller1@gmail.com>
2026-04-23 16:16:35 -0700

    setup and launch files

---
+++ ros_packages/seed_controller_ws/src/demo_night/launch/launch.py
@@ -0,0 +1,69 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        # The master multiplexer
+        Node(
+            package='demo_night',
+            executable='commander_node',
+            name='commander_node',
+            output='screen'
+        ),
+        
+        # The PyQt6 Interface and Virtual Twin
+        Node(
+            package='demo_night',
+            executable='gui_node',
+            name='gui_node',
+            output='screen'
+        ),
+        
+        # Hardware Motor Driver Interface
+        Node(
+            package='demo_night',
+            executable='gtg_motor_node',
+            name='gtg_motor_node',
+            output='screen'
+        ),
+        
+        # ArUco Fiducial Rehoming Sequence
+        Node(
+            package='demo_night',
+            executable='aruco_rehoming_node',
+            name='aruco_rehoming_node',
+            output='screen'
+        ),
+        
+        # Blue Tape Optical Path Follower
+        Node(
+            package='demo_night',
+            executable='optical_path_follower_node',
+            name='optical_path_follower_node',
+            output='screen'
+        ),
+        
+        # Red Mask Go-To-Goal (Highest Priority Override)
+        Node(
+            package='demo_night',
+            executable='gtg_controller_node',
+            name='gtg_controller_node',
+            output='screen'
+        ),
+        
+        # Realsense controlling node
+        Node(
+            package='demo_night',
+            executable='realsense_node',
+            name='realsense_node',
+            output='screen'
+        ),
+        # Cam display node
+        Node(
+            package='demo_night',
+            executable='display_node',
+            name='display_node',
+            output='screen'
+        ),
+    ])
--- ros_packages/seed_controller_ws/src/demo_night/setup.py
+++ ros_packages/seed_controller_ws/src/demo_night/setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'demo_night'
 
@@ -10,6 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # Include all launch files
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.py')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,6 +24,13 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'commander_node = demo_night.commander_node:main',
+            'gui_node = demo_night.gui_node:main',
+            'aruco_rehoming_node = demo_night.aruco_rehoming_node:main',
+            'optical_path_follower_node = demo_night.optical_path_follower_node:main',
+            'gtg_controller_node = demo_night.gtg_controller_node:main',
+            'realsense_node = demo_night.realsense_node:main',
+            'display_node = demo_night:display_node:main',
         ],
     },
 )
